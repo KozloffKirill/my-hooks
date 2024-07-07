f25f6a48612c294bc46269ff101abfab45c8ac18
Кирилл <kirillkozlove39@gmail.com>
2024-07-07 17:31:06 +0300

    feat: app init

---
+++ webpack.config.ts
@@ -0,0 +1,68 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import 'webpack-dev-server'; //https://webpack.js.org/configuration/configuration-languages/#typescript
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+type EnvType = {
+  mode: 'production' | 'development';
+  port: number;
+};
+
+const config: (env: EnvType) => webpack.Configuration = ({
+  mode = 'development',
+  port = 3000,
+}) => ({
+  mode,
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: mode === 'development' ? '[path][name]__[local]' : '[hash:base64]',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new MiniCssExtractPlugin()],
+  devServer: {
+    port,
+    static: './build',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      '@shared': path.resolve(__dirname, 'src', 'shared'),
+      '@entities': path.resolve(__dirname, 'src', 'entities'),
+      '@features': path.resolve(__dirname, 'src', 'features'),
+      '@widgets': path.resolve(__dirname, 'src', 'widgets'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
+      '@app': path.resolve(__dirname, 'src', 'app'),
+    },
+  },
+});
+
+export default config;
