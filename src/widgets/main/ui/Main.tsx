d26904b6d95eb091e094310dbfbc0689fb1ff082
Кирилл <kirillkozlove39@gmail.com>
2024-07-07 19:46:55 +0300

    feat: <Main />

--- src/widgets/main/ui/Main.tsx
+++ src/widgets/main/ui/Main.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import sts from './Main.module.css';
+import { Link } from 'react-router-dom';
 
 export const Main: FC = () => {
   return (
     <section className={sts.main}>
       <div className={sts.container}>
-        <h1>My hooks</h1>
-        <p>React hooks by Kozlov Kirill</p>
+        <h1 className={sts.header}>My hooks</h1>
+        <p className={sts.description}>React hooks by Kozlov Kirill</p>
+        <Link to="hooks" className={sts.link}>
+          Explore the hooks
+        </Link>
       </div>
     </section>
   );
